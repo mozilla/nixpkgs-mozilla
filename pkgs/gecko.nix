8b4faa0bc749324b57975d014cd2692bb3d0b519
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2016-07-08 16:11:29 +0000

    Update the mozconfig content to compile with the latest modification of the build system.

--- pkgs/gecko.nix
+++ pkgs/gecko.nix
@@ -51,19 +51,19 @@ stdenv.mkDerivation {
   configurePhase = ''
     export MOZBUILD_STATE_PATH=$(pwd)/.mozbuild
     export MOZ_CONFIG=$(pwd)/.mozconfig
-    export builddir=$(pwd)/build
+    export builddir=$(pwd)/builddir
 
     mkdir -p $MOZBUILD_STATE_PATH $builddir
     echo > $MOZ_CONFIG "
     . $src/build/mozconfig.common
 
+    mk_add_options MOZ_OBJDIR=$builddir
+    mk_add_options AUTOCONF=${autoconf213}/bin/autoconf
     ac_add_options --prefix=$out
     ac_add_options --enable-application=browser
     ac_add_options --enable-official-branding
-    "
-
-    # Make sure mach can find autoconf 2.13, as it is not suffixed in Nix.
     export AUTOCONF=${autoconf213}/bin/autoconf
+    "
   '';
 
   AUTOCONF = "${autoconf213}/bin/autoconf";
