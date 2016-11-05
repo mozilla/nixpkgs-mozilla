749e82acdcce0fd0612901e8b400e765153ae6ad
zimbatm <zimbatm@zimbatm.com>
2016-11-05 16:15:19 +0000

    firefox-dev-bin: use stable source
    Change the generation script to discover and pin the date and version of
    the release.

--- pkgs/firefox-dev-bin/dev_sources.nix
+++ pkgs/firefox-dev-bin/dev_sources.nix
@@ -1,12 +1,12 @@
 # This file is generated from generate_sources_dev.rb. DO NOT EDIT.
 # Execute the following command to update the file.
 #
-# ruby generate_sources_dev.rb 49.0a2 > dev_sources.nix
+# ruby generate_sources_dev.rb > dev_sources.nix
 
 {
   version = "51.0a2";
   sources = [
-    { url = "http://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-aurora/firefox-51.0a2.en-US.linux-i686.tar.bz2"; locale = "en-US"; arch = "linux-i686"; sha512 = "19c8cfcdc4aae293f577a885ecf500820bc1f8f4e3a1aed705266ac0127079ecc1f6331601ba2d7ac374dbb6498f53d10569eeab6bd676320e5d99a494cb47ad"; }
-    { url = "http://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-aurora/firefox-51.0a2.en-US.linux-x86_64.tar.bz2"; locale = "en-US"; arch = "linux-x86_64"; sha512 = "4b58d73a692d24d31f30241083cd0842af7f54dc642a6b4c056782ec83565454d3337c514d60d1291f0ab51b331e5089122d967a06da17fdb186229dfa12f579"; }
+    { url = "http://download-installer.cdn.mozilla.net/pub/firefox/nightly/2016/11/2016-11-05-00-40-17-mozilla-aurora/firefox-51.0a2.en-US.linux-i686.tar.bz2"; locale = "en-US"; arch = "linux-i686"; sha512 = "2a0a094c673455c562100bda7ea288463570907a7443316849785607a16bbd2658ef9fdc438f44eeceb9cc24157aa76ca7900c7fe6d4143a7c18bd8ccc2a6b1e"; }
+    { url = "http://download-installer.cdn.mozilla.net/pub/firefox/nightly/2016/11/2016-11-05-00-40-17-mozilla-aurora/firefox-51.0a2.en-US.linux-x86_64.tar.bz2"; locale = "en-US"; arch = "linux-x86_64"; sha512 = "66084233f082a11189be080b37cacf7431c36d9fba6d1d8229fb74ad205061d533c8543db7c668e6f630270f7865d46edc023aac29a30cc1a9e0e84a9aaf2bdd"; }
   ];
 }
