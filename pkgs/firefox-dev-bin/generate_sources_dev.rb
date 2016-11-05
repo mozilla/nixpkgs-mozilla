aa0827a7602c332bc7756c4141ab5ce638e7e84b
zimbatm <zimbatm@zimbatm.com>
2016-11-05 16:15:19 +0000

    firefox-dev-bin: use stable source
    Change the generation script to discover and pin the date and version of
    the release.

--- pkgs/firefox-dev-bin/generate_sources_dev.rb
+++ pkgs/firefox-dev-bin/generate_sources_dev.rb
@@ -1,27 +1,34 @@
 #!/usr/bin/env ruby
 require "open-uri"
 
-version =
-  if ARGV.empty?
-    $stderr.puts("Usage: ruby generate_sources_dev.rb <version> > dev_sources.nix")
-    exit(-1)
-  else
-    ARGV[0]
-  end
+Source = Struct.new(:hash, :arch, :locale, :filename)
 
-base_url = "http://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-aurora"
-
+base_uri = URI.parse("http://download-installer.cdn.mozilla.net/pub/firefox/nightly/")
 arches = ["linux-i686", "linux-x86_64"]
 locales = ["en-US"]
-sources = []
 
-Source = Struct.new(:hash, :arch, :locale, :filename)
+# Find latest release
+year_uri = base_uri.merge(
+  open(base_uri).read.scan(/#{base_uri.path}\d{4}\//).last
+)
+month_uri = year_uri.merge(
+  open(year_uri).read.scan(/#{year_uri.path}\d{2}\//).last
+)
+day_uri = month_uri.merge(
+  open(month_uri).read.scan(/#{month_uri.path}[^\/]+aurora\//).last
+)
+base_url = day_uri
+
+version = open(base_url).read.match("/firefox-([^-]+).#{locales.first}")[1]
+
+sources = []
 
 locales.each do |locale|
   arches.each do |arch|
     basename = "firefox-#{version}.#{locale}.#{arch}"
     filename = basename + ".tar.bz2"
-    sha512 = open("#{base_url}/#{basename}.checksums").each_line
+    url = base_url.merge("#{basename}.checksums")
+    sha512 = open(url).each_line
       .find(filename).first
       .split(" ").first
     sources << Source.new(sha512, arch, locale, filename)
@@ -36,7 +43,7 @@ puts(<<"EOH")
 # This file is generated from generate_sources_dev.rb. DO NOT EDIT.
 # Execute the following command to update the file.
 #
-# ruby generate_sources_dev.rb 49.0a2 > dev_sources.nix
+# ruby generate_sources_dev.rb > dev_sources.nix
 
 {
   version = "#{version}";
@@ -44,7 +51,7 @@ puts(<<"EOH")
 EOH
 
 sources.each do |source|
-  puts(%Q|    { url = "#{base_url}/#{source.filename}"; locale = "#{source.locale}"; arch = "#{source.arch}"; sha512 = "#{source.hash}"; }|)
+  puts(%Q|    { url = "#{base_url.merge(source.filename)}"; locale = "#{source.locale}"; arch = "#{source.arch}"; sha512 = "#{source.hash}"; }|)
 end
 
 puts(<<'EOF')
