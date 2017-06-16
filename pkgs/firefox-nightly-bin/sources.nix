c256d490d39201ad63757d6925a92d18639c38ee
Rok Garbas <rok@garbas.si>
2017-06-16 13:56:52 +0200

    firefox-nightly-bin: update

--- pkgs/firefox-nightly-bin/sources.nix
+++ pkgs/firefox-nightly-bin/sources.nix
@@ -1,10 +1,10 @@
 {
-  version = "56.0a1" + "-" + "20170614100332";
+  version = "56.0a1" + "-" + "20170616100254";
   sources = [
     { url = "http://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-56.0a1.en-US.linux-x86_64.tar.bz2";
       locale = "en-US";
       arch = "linux-x86_64";
-      sha512 = "3acd16bde75e4f79717da1c19f2e326df1bb84f82d117200ed2f55dd28165cd39d65e45bc9d95b07fe61ca77ef9b8a2aa385f19b782db13f7a80244ab2a198ca";
+      sha512 = "01fe9102bea366bd9a9e8328ceef00c8b651657ca6fdfce1452fb50e6187ae2bd19bdbb0b6a42db44729a642c343c4520b52366e4449f3d1043bd1924b6c9b9a";
     }
   ];
 }
