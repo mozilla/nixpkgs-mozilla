b475cacb41cbf99751a0323e4b2f9215827a13a9
adisbladis <adis@blad.is>
2017-07-12 09:49:34 +0800

    firefox-nightly-bin: 56.0a1-20170705170343 -> 56.0a1-20170711100226

--- pkgs/firefox-nightly-bin/sources.nix
+++ pkgs/firefox-nightly-bin/sources.nix
@@ -1,10 +1,10 @@
 {
-  version = "56.0a1" + "-" + "20170705170343";
+  version = "56.0a1" + "-" + "20170711100226";
   sources = [
     { url = "http://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-56.0a1.en-US.linux-x86_64.tar.bz2";
       locale = "en-US";
       arch = "linux-x86_64";
-      sha512 = "2be1afd837f41b2f6781760801cc8344ad5add9d03e5844e819d92980c7901c4a2fa1b890e8bc01528393b42c3abce3dd00901f7a5cefbda953858e8de219be8";
+      sha512 = "3a5474497a9c9d19c2c92533cf25468b5b1246e16bc1d1876f6256da6df44fb29abd2e563c6ed05b5674d1839e0587d31c4b9773028a78f46f463d4ed62d0428";
     }
   ];
 }
