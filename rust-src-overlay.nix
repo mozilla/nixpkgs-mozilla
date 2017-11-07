c62285b5eb5163cf05ace86b22067b92f5415709
Peter Kolloch <info@eigenvalue.net>
2017-11-07 10:33:58 +0100

    Add a rust-src-overlay.nix which adds rust-src as an extension to all channels.

---
+++ rust-src-overlay.nix
@@ -0,0 +1,18 @@
+# Overlay that builds on top of rust-overlay.nix.
+# Adds rust-src component to all channels which is helpful for racer, intellij, ...
+
+self: super:
+
+let mapAttrs = super.stdenv.lib.mapAttrs;
+    flip = super.stdenv.lib.flip;
+in {
+  # install stable rust with rust-src:
+  # "nix-env -i -A nixos.latest.rustChannels.stable.rust"
+
+  latest.rustChannels =
+    flip mapAttrs super.latest.rustChannels (name: value: value // {
+      rust = value.rust.override {
+        extensions = ["rust-src"];
+      };
+    });
+}
