ae60921101ad2e01870ae70ea6b16bbbc50b7d12
Rail Aliiev <rail@mozilla.com>
2018-04-04 07:09:56 -0400

    Update Vidyo Version

--- pkgs/VidyoDesktop/default.nix
+++ pkgs/VidyoDesktop/default.nix
@@ -4,19 +4,22 @@
 }:
 
 let
+  vidyoVersion = "3.6.3";
+  vidyoBuild = "017";
+  vidyoVersionUnderscore = builtins.replaceStrings ["."] ["_"] vidyoVersion;
   VidyoDesktopDeb = stdenv.mkDerivation {
-    name = "VidyoDesktopDeb-123";
+    name = "VidyoDesktopDeb-${vidyoVersion}";
     builder = ./builder.sh;
     inherit dpkg;
     src = fetchurl {
-      url = "https://v.mozilla.com/upload/VidyoDesktopInstaller-ubuntu64-TAG_VD_3_3_0_027.deb";
-      sha256 = "045f9z421qpcm45bmh98f3h7bd46rdjvcbdpv4rlw9ribncv66dc";
+      url = "https://v.mozilla.com/upload/VidyoDesktopInstaller-ubuntu64-TAG_VD_${vidyoVersionUnderscore}_${vidyoBuild}.deb";
+      sha256 = "01spq6r49myv82fdimvq3ykwb1lc5bymylzcydfdp9xz57f5a94x";
     };
     buildInputs = [ makeWrapper ];
   };
 
 in buildFHSUserEnv {
-  name = "VidyoDesktop-123";
+  name = "VidyoDesktop";
   targetPkgs = pkgs: [ VidyoDesktopDeb ];
   multiPkgs = pkgs: [
     patchelf dpkg alsaLib alsaUtils alsaOss alsaTools alsaPlugins
