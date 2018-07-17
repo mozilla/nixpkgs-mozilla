8b2a2a712f4b692cfceee3e089ab5ea48c79104f
Bastian Köcher <git@kchr.de>
2018-07-17 12:39:11 +0200

    VidyoDesktop: Fixes missing libidn by using 1.34

--- pkgs/VidyoDesktop/default.nix
+++ pkgs/VidyoDesktop/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, fetchurl, buildFHSUserEnv, makeWrapper, dpkg, alsaLib,
-  alsaUtils, alsaOss, alsaTools, alsaPlugins, libidn, libGL, utillinux, mesa_glu, qt4,
+{ stdenv, pkgs, fetchurl, buildFHSUserEnv, makeWrapper, dpkg, alsaLib,
+  libGL, alsaUtils, alsaOss, alsaTools, alsaPlugins, utillinux, mesa_glu, qt4,
   zlib, patchelf, xorg, libpulseaudio
 }:
 
@@ -17,8 +17,53 @@ let
     };
     buildInputs = [ makeWrapper ];
   };
-
-in buildFHSUserEnv {
+  libidn = stdenv.mkDerivation rec {
+    name = "libidn-1.34";
+  
+    src = fetchurl {
+      url = "mirror://gnu/libidn/${name}.tar.gz";
+      sha256 = "0g3fzypp0xjcgr90c5cyj57apx1cmy0c6y9lvw2qdcigbyby469p";
+    };
+  
+    outputs = [ "bin" "dev" "out" "info" "devdoc" ];
+  
+    # broken with gcc-7
+    #doCheck = !stdenv.isDarwin && !stdenv.hostPlatform.isMusl;
+  
+    hardeningDisable = [ "format" ];
+  
+    buildInputs = stdenv.lib.optional stdenv.isDarwin pkgs.libiconv;
+  
+    doCheck = false; # fails
+  
+    meta = {
+      homepage = http://www.gnu.org/software/libidn/;
+      description = "Library for internationalized domain names";
+  
+      longDescription = ''
+        GNU Libidn is a fully documented implementation of the
+        Stringprep, Punycode and IDNA specifications.  Libidn's purpose
+        is to encode and decode internationalized domain names.  The
+        native C, C\# and Java libraries are available under the GNU
+        Lesser General Public License version 2.1 or later.
+  
+        The library contains a generic Stringprep implementation.
+        Profiles for Nameprep, iSCSI, SASL, XMPP and Kerberos V5 are
+        included.  Punycode and ASCII Compatible Encoding (ACE) via IDNA
+        are supported.  A mechanism to define Top-Level Domain (TLD)
+        specific validation tables, and to compare strings against those
+        tables, is included.  Default tables for some TLDs are also
+        included.
+      '';
+  
+      repositories.git = git://git.savannah.gnu.org/libidn.git;
+      license = stdenv.lib.licenses.lgpl2Plus;
+      platforms = stdenv.lib.platforms.all;
+      maintainers = [ ];
+    };
+  };
+in
+buildFHSUserEnv {
   name = "VidyoDesktop";
   targetPkgs = pkgs: [ VidyoDesktopDeb ];
   multiPkgs = pkgs: [
