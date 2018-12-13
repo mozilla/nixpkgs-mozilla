fcd3e318d6e90ea8ab30951431ce9a6efda42566
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-12-13 09:07:13 -0500

    Bump cbindgen to 0.6.7
    This is required now since
    https://bugzilla.mozilla.org/show_bug.cgi?id=1503401.

--- pkgs/cbindgen/default.nix
+++ pkgs/cbindgen/default.nix
@@ -5,13 +5,13 @@
 
 rustPlatform.buildRustPackage rec {
   name = "rust-cbindgen-${version}";
-  version = "0.6.6";
+  version = "0.6.7";
 
   src = fetchFromGitHub {
     owner = "eqrion";
     repo = "cbindgen";
     rev = "v${version}";
-    sha256 = "12s4lps8p6hrxvki9a5dxzbmxsddvkm6ias3n4daa1s3bncd86zk";
+    sha256 = "0sgkgvkqrc6l46fvk6d9hsy0xrjpl2ix47f3cv5bi74dv8i4y2b4";
   };
 
   cargoSha256 = "137dqj1sp02dh0dz9psf8i8q57gmz3rfgmwk073k7x5zzkgvj21c";
