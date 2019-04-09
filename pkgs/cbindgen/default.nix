0bc9aeaf007c75b5bee741438829d8458dedebb2
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-04-09 10:59:48 -0400

    Update cbindgen to 0.8.3

--- pkgs/cbindgen/default.nix
+++ pkgs/cbindgen/default.nix
@@ -5,13 +5,13 @@
 
 rustPlatform.buildRustPackage rec {
   name = "rust-cbindgen-${version}";
-  version = "0.8.2";
+  version = "0.8.3";
 
   src = fetchFromGitHub {
     owner = "eqrion";
     repo = "cbindgen";
     rev = "v${version}";
-    sha256 = "1ck0zyhrrj61rxcmz4045m4nl04g6r971min5hz5p8cmx4h5gl9w";
+    sha256 = "08zlnk1k1nddjciccfdcplxqngsnz6ml3zxm57mijabzybry8zz1";
   };
 
   cargoSha256 = "00j5nm491zil6kpjns31qyd6z7iqd77b5qp4h7149s70qjwfq2cb";
