d392a1a295223447334cc53393827e0e5062e5df
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-05-09 18:43:13 -0400

    Update phlay to 0.2.0

--- pkgs/phlay/default.nix
+++ pkgs/phlay/default.nix
@@ -3,12 +3,12 @@
 }:
 python36Packages.buildPythonApplication {
   name = "phlay";
-  version = "0.1.5";
+  version = "0.2.0";
   src = fetchFromGitHub {
     owner = "mystor";
     repo = "phlay";
-    rev = "da238512d89eacda526a4f53fc0a096ad594efd9";
-    sha256 = "1cq1jq89xwx25yyqa4n4jhy55bvbyqm0knd7m8vacivqn9p3krks";
+    rev = "398fccce05ce31d7d731d437c7febf30d3d83bf4";
+    sha256 = "10mp941g639vgmiqpjdpfyazcwlvhj84ad78axn2waq20mi60l88";
   };
   meta = {
     description = "A command-line interface for Phabricator";
