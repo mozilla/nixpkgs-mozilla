66b51edb0b15aad70d2fa8ae9baae34c4eb9719e
James  Kay <twey@twey.co.uk>
2021-07-15 15:22:11 +0100

    Fix `rr-overlay.nix` for modern nixpkgs
    Fixes the error:
    ```
    error: anonymous function at /nix/store/lh73pszzv2569h92dn8n56cccdxyvn9g-nixos-21.11pre301985.23cd13167a1/nixos/pkgs/development/tools/analysis/rr/default.nix:1:1 called with unexpected argument 'stdenv', at /nix/store/lh73pszzv2569h92dn8n56cccdxyvn9g-nixos-21.11pre301985.23cd13167a1/nixos/lib/customisation.nix:69:16
    ```
    `rr` in nixpkgs now expects a `gcc9Stdenv` argument instead of a `stdenv` argument.

--- rr-overlay.nix
+++ rr-overlay.nix
@@ -3,8 +3,8 @@ self: super:
 {
   # Add i686-linux platform as a valid target.
   rr = super.rr.override {
-    stdenv = self.stdenv // {
-      mkDerivation = args: self.stdenv.mkDerivation (args // {
+    gcc9Stdenv = self.gcc9Stdenv // {
+      mkDerivation = args: self.gcc9Stdenv.mkDerivation (args // {
         meta = args.meta // {
           platforms = self.lib.platforms.linux;
         };
